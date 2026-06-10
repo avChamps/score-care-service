f5bf82f21cca556c2039f32a51a8ae7f192a0c91
Disendra Yadav <disendra123@gmail.com>
2026-06-10 17:53:45 +0530

    cors issue fix

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const {
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: env.corsOrigin, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
